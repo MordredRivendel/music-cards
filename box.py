13dc95b157c8e18bfba396e9a481c5d36f56343d
MordredRivendel <44966835+MordredRivendel@users.noreply.github.com>
2019-01-22 10:27:04 +0100

    Update box.py

--- box.py
+++ box.py
@@ -45,14 +45,6 @@ reader = Reader()
 cardList = CardList()
 
 print 'Ready: place a card on top of the reader'
-autostart = 'file:///var/lib/mopidy/network/start.mp3'    # autostart funktion
-if autostart != '':
-	client = connectMPD()
-	client.stop()
-	client.clear()
-	client.add(autostart)
-	client.play()
-	client.close()
 
 while True:
 	try:
